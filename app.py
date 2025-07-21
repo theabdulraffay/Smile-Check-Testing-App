18dc2c7087ff9c9168a03376753f3fb75e4c6adf
Abdul Raffay <81872649+theabdulraffay@users.noreply.github.com>
2025-07-22 00:02:07 +0500

    fix: image uploader

--- app.py
+++ app.py
@@ -14,19 +14,19 @@ if first is not None:
     st.image(first, caption="Uploaded Image")
 
 
-sec = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"], key="second")
+# sec = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"], key="second")
 
-if sec is not None:
-    st.image(sec, caption="Uploaded Image")
-
-
-third = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"], key="third")
-
-if third is not None:
-    st.image(third, caption="Uploaded Image")
+# if sec is not None:
+#     st.image(sec, caption="Uploaded Image")
 
 
-if first is not None and sec is not None and third is not None:
+# third = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"], key="third")
+
+# if third is not None:
+#     st.image(third, caption="Uploaded Image")
+
+
+if first is not None:
     st.success("All three images have been uploaded successfully!")
 
     # Show button and wait for click
@@ -43,14 +43,14 @@ if first is not None and sec is not None and third is not None:
                     data=first.getvalue(),
                     mime_type=first.type,
                 ),
-                types.Part.from_bytes(
-                    data=sec.getvalue(),
-                    mime_type=sec.type,
-                ),
-                types.Part.from_bytes(
-                    data=third.getvalue(),
-                    mime_type=third.type,
-                ),
+                # types.Part.from_bytes(
+                #     data=sec.getvalue(),
+                #     mime_type=sec.type,
+                # ),
+                # types.Part.from_bytes(
+                #     data=third.getvalue(),
+                #     mime_type=third.type,
+                # ),
 
                 'You are a dental health care assistant. Check the dental situation and give a score between 1- 10. Also analyze the teeths and give your feedback or any tips for the future. Keep your answer short and concise. \n If the images is not clear, say "Image not clear". or if image is not related to dental situation, say "Image not related to dental situation".'
             ]
